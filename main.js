37f6e509b203c422d66cf2240781aa960ea31ba4
NOBUOSAN <gariben.elsha.a27scrv@gmail.com>
2020-04-24 20:41:38 +0900

    second commit ,miss

--- main.js
+++ main.js
@@ -18,4 +18,22 @@ new Vue({
       this.items = _.shuffle(this.items)
     }
   }
+});
+
+
+new Vue({
+  el: "#sudoku-demo",
+  data: {
+    cells: Array.apply(null, { length: 81 }).map(function(_, index) {
+      return {
+        id: index,
+        number: (index % 9) + 1
+      };
+    })
+  },
+  methods: {
+    shuffle: function() {
+      this.cells = _.shuffle(this.cells);
+    }
+  }
 })
